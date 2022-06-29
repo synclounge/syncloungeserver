a5d402109be49e3087a38ecfb7928e2c69588686
marissa999 <github@littles.moe>
2022-06-29 13:37:12 +0000

    Replacing url-join with node:url

--- src/socketserver/index.js
+++ src/socketserver/index.js
@@ -3,8 +3,8 @@
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
-import urljoin from 'url-join';
 
+import { URL } from 'node:url';
 import { Server } from 'socket.io';
 import attachEventHandlers from './handlers';
 
@@ -25,7 +25,7 @@ const socketServer = ({
   app.use(baseUrl, router);
 
   const socketio = new Server(server, {
-    path: urljoin(baseUrl, '/socket.io'),
+    path: new URL('socket.io', baseUrl).toString(),
     cors: {
       origin: '*',
     },
